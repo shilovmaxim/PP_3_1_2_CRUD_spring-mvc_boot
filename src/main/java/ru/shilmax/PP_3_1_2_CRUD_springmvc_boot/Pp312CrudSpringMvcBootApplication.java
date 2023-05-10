1a042c3d56d90a94cb18c2f46bc7b484701756ec
shilovmaxim <shilovmaxim@gmail.com>
2023-05-10 17:18:43 +0300

    Initial commit

---
+++ src/main/java/ru/shilmax/PP_3_1_2_CRUD_springmvc_boot/Pp312CrudSpringMvcBootApplication.java
@@ -0,0 +1,13 @@
+package ru.shilmax.PP_3_1_2_CRUD_springmvc_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp312CrudSpringMvcBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp312CrudSpringMvcBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shilmax/PP_3_1_2_CRUD_springmvc_boot/Pp312CrudSpringMvcBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shilmax.PP_3_1_2_CRUD_springmvc_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp312CrudSpringMvcBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
